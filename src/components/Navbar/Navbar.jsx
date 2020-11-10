84230fba6e83dfff0d5c95ffdfce5986ed6f0010
Kymani N Hanson <kymanihanson@gmail.com>
2020-11-09 20:11:20 -0800

    Updated the Nav Bar Router to use the Nav Links
    React Router functionality was added in the last commit but the links in the actual navigation bar did not have them. I added it to the links for a fully functioning nav bar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,39 @@
 import React from "react";
 
+import { Link } from 'react-router-dom';
+
 import "./Navbar.css";
 
 function Navbar() {
+
+  const navLinkStyle = {
+    textDecoration: 'none'
+  }
+
+  const logoPath = "https://www.flaticon.com/svg/static/icons/svg/3706/3706707.svg";
+
     return (
       <nav className="navbar">
         <div className="logoSec">
-          <img
-            src="https://www.flaticon.com/svg/static/icons/svg/3706/3706707.svg"
-            alt=""
-          />
+          <img src={logoPath} alt="Logo" id="logoImage"/>
         </div>
         <div className="menuSec">
           <ul className="navMenu">
-            <li>
-              <a href="#Home">Home</a>
-            </li>
-            <li>
-              <a href="#Courses">Courses</a>
-            </li>
-            <li>
-              <a href="#About">About</a>
-            </li>
-            <li>
-              <a href="#Contact">Contact</a>
-            </li>
+            <Link to='/' style={navLinkStyle}>
+              <li> Home </li>
+            </Link>
+
+            <Link to='/courses' style={navLinkStyle}>
+              <li> Courses </li>
+            </Link>
+
+            <Link to='/about' style={navLinkStyle}>
+              <li> About </li>
+            </Link>
+            
+            <Link to='/contact' style={navLinkStyle}>
+              <li> Contact </li>
+            </Link>
           </ul>
         </div>
           <div className="hamburger"> </div>
